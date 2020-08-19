af524e415bf45c08b7c365bbaa8b6caada5e5180
Ralvarez <R.alvgar@hotmail.com>
2020-08-19 11:58:10 +0200

    link repair

--- loading.js
+++ loading.js
@@ -1,5 +1,5 @@
 function start(){
-
+  $("#button").css("visibility", "hidden");
      var duration = 5000; // it should finish in 5 seconds !
      var percent = duration / 100; // 1 percent of duration
      var i = 0 ;
@@ -12,6 +12,7 @@ function start(){
         if(i>=100){
             clearInterval(interval);
             $("#msg").text("Done!");
+            $("#button").css("visibility", "visible");
         }
     }, percent);
 }
