d67a4bcfb9c6bc1f30e10bad4e4ea5beb76095cb
Ralvarez <R.alvgar@hotmail.com>
2020-08-31 08:47:54 +0200

    ES6

--- loading.js
+++ loading.js
@@ -1,16 +1,15 @@
-function start(){
+const start = () => {
     document.getElementById('button').setAttribute("disabled", "");
-
-    var duration = 5000; // it should finish in 5 seconds
-    var percent = duration / 100; // 1 percent of duration
-    var i = 0 ;
-    var interval = setInterval(function(){
+    const duration = 5000; // it should finish in 5 seconds
+    const percent = duration / 100; // 1 percent of duration
+    let i = 0 ;
+    const interval = setInterval( () => {
       i++;
       document.getElementById('box').style.width = i + "%";
       document.getElementById('box').style.opacity = 1;
       document.getElementById('prc').textContent = i + "%";
       document.getElementById('msg').textContent = "Loading...";
-      if(i>=100){
+      if (i>=100) {
           clearInterval(interval);
           document.getElementById('msg').textContent = "Done!";
           document.getElementById('button').removeAttribute("disabled", "");
